e73cbffda9ef572b7dc8a3c55da44c61000ede3a
madeleinel <madeleinelinder@hotmail.com>
2017-03-24 21:30:00 +0000

    scaled back code: to add specific commands once we have a database to call on

--- twitterbot/twitterbot.py
+++ twitterbot/twitterbot.py
@@ -7,4 +7,12 @@ auth.set_access_token("845260665115885568-N35GJTJiXAtInYVig4qQ0NRy8jpNWy2", "7iU
 twitter_api = tweepy.API(auth)
 
 # To tweet "Happy Friday evening!":
-twitter_api.update_status("Happy Friday evening!")
+# twitter_api.update_status("Happy Friday evening!")
+
+# To retweet a tweet with ID 844128496188375041 (one of Madde's)
+# twitter_api.retweet(844128496188375041)
+
+# To print within the command line: the user name of the authors for the 15 most recent tweets featuring the term "womentor"
+# wtt_tweets = twitter_api.search( q="womentor" )
+# for tweet in wtt_tweets:
+#     print "tweet by " + tweet.user.name
