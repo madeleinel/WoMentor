40aed3c18943f26489947077c1d4c6b5e86f39d8
eleanor keane <egjkeane@gmail.com>
2017-03-31 12:55:22 +0100

    add changes to tweet parser

--- twitter/tweetparse.py
+++ twitter/tweetparse.py
@@ -1,4 +1,5 @@
 import re
+from acceptedlangs import accepted_langs_normal, accepted_langs_lower
 
 def tweetParse(tweetString):
     # first identify if mentor or mentee
@@ -16,18 +17,64 @@ def tweetParse(tweetString):
     substring_3 = tweetString[index_3:tweetStringLength]
     substring_1 = toMultiStrings(substring_1)
     substring_2 = toMultiStrings(substring_2)
-    substring_3 = toMultiStrings(substring_3)
-
+    substring_3 = toMultiStrings(substring_3, isOffer=True)
+    substring_1, substring_2 = checkTheLists(substring_1, substring_2)
+    substring_3 = checkOffers(substring_3)
     return isMentor, substring_1, substring_2, substring_3
 
-def toMultiStrings(string):
+def toMultiStrings(string, isOffer=False):
+  if isOffer:
+    string = re.sub('-(.*?):', '', string)
+    string = string.split(',')
+    string = string.strip() if type(string) is str else stripString(string)
+    return string
+  else:
     string = re.sub('-(.*?):', '', string)
     string = re.sub(',', '', string)
     string = string.strip()
     string = string.split()
-    for str in string:
-        str = str.strip()
     return string
+
+def stripString(string):
+  for indx, strng in enumerate(string):
+    strng = strng.strip()
+    string[indx] = strng
+  return string
+
+
+def checkTheLists(langstore, skillstore):
+    langs = []
+    skills = []
+    for language in langstore:
+        if language.lower() in accepted_langs_lower:
+            langs.append(language)
+        elif 'SQL' in language:
+            langs.append(language)
+        else:
+            skills.append(language)
+    if len(langs) > 4:
+        langs = langs[:3]
+        langs.append('and more...')
+    if len(skillstore) <= 4:
+        if len(skills) != 0:
+            for skill in skills:
+                skillstore.append(skill)
+    if len(skillstore) > 4:
+        print skillstore
+        skillstore = skillstore[:3]
+        skillstore.append('and more...')
+    return langs, skillstore
+
+def checkOffers(offerstore):
+    offers = []
+    for offer in offerstore:
+      if 'start' in offer:
+        offers.append('getting started')
+      elif 'career' in offer:
+        offers.append('career advice')
+      elif 'project' in offer:
+        offers.append('project help')
+    return offers
 
 # example tweets:
 # #WomenToTech #Mentor -langs: javascript, python, haskell -skill: node.js, d3.js, jinja2 -offering: help getting started
