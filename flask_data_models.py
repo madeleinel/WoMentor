2db9f66d1a2c7148fa4a3838a00105d338aee76e
eleanor keane <egjkeane@gmail.com>
2017-04-03 22:06:16 +0100

    add pretty essential __repr__ function to each class so they don't return random memory addresses

--- flask_data_models.py
+++ flask_data_models.py
@@ -11,12 +11,18 @@ class User(db.Model):
     skills = db.relationship('Skills', backref='skillset', lazy='dynamic')
     languages = db.relationship('Languages', backref='polyglot', lazy='dynamic')
 
+    def __repr__(self):
+        return "<User(uid={}, scrn_name={}, offer={}, langs={}, skills={}, originaltweet={}, twitteruid={})>".format(self.uid, self.scrn_name, self.offer, self.languages, self.skills, self.original_tweet_id, self.twitter_uid)
+
 class Offer(db.Model):
     oid = db.Column(db.Integer, primary_key=True)
     offer_1 = db.Column(db.String(100))
     offer_2 = db.Column(db.String(100))
     offer_3 = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.uid'))
+
+    def __repr__(self):
+        return "Offer 1: {}, 2: {}, 3: {}".format(self.offer_1, self.offer_2, self.offer_3)
 
 class Skills(db.Model):
     oid = db.Column(db.Integer, primary_key=True)
@@ -26,6 +32,9 @@ class Skills(db.Model):
     skills_4 = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.uid'))
 
+    def __repr__(self):
+        return "Skills 1: {}, 2: {}, 3: {}, 4: {}".format(self.skills_1, self.skills_2, self.skills_3, self.skills_4)
+
 class Languages(db.Model):
     oid = db.Column(db.Integer, primary_key=True)
     languages_1 = db.Column(db.String(100))
@@ -33,3 +42,6 @@ class Languages(db.Model):
     languages_3 = db.Column(db.String(100))
     languages_4 = db.Column(db.String(100))
     user_id = db.Column(db.Integer, db.ForeignKey('user.uid'))
+
+    def __repr__(self):
+        return "Languages 1: {}, 2: {}, 3: {}, 4: {}".format(self.languages_1, self.languages_2, self.languages_3, self.languages_4)
