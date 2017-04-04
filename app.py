08f7ef087a9399dec5684066c756e7e290135b2c
madeleinel <madeleinelinder@hotmail.com>
2017-04-04 18:43:23 +0100

    Started creating the mentee and mentor profile pages: So far have just set up the paths in app.py and created basic HTML files

--- app.py
+++ app.py
@@ -26,22 +26,36 @@ def create_app():
 def main():
     return render_template("index.html")
 
+# to show the About page when on the "/about" URL
+@app.route("/about")
+def showAboutPage():
+    return render_template("about.html")
+
 # to show the Mentee sign-up page when on the "/mentee_signup" URL
 @app.route("/mentee_signup")
 def showMenteeSignup():
-    return render_template("menteeSignup.html")
+    return render_template("signup.html")
 
 # to show the Mentor sign-up page when on the "/mentor_signup" URL
 @app.route("/mentor_signup")
 def showMentorSignup():
-    return render_template("mentorSignup.html")
+    return render_template("signup.html")
+
+##### for the profile pages: details & db paths need to be updated once the database is done
+
+# to show a list of all mentee profiles
+@app.route("/mentees")
+def showMenteeList():
+    mentordict = [{ "twitterhandle": "fluffyunicorn", "languages": "javascript, python, ruby", "skills": "tight-rope walking, laughter", "offers": "getting started, happy unicorns", "originaltweet": "http://twitter.com/anyuser/status/203490203491094", "twitterprofile": "https://twitter.com/intent/user?user_id=23492" }, { "twitterhandle": "madeilenel", "languages": "javascript, python", "offers": "getting started, fika", "skills": "tea breaking, cartwheels", "originaltweet": "http://twitter.com/anyuser/status/203490203491094", "twitterprofile": "https://twitter.com/intent/user?user_id=23492" }]
+    # TO DO: adapt this to work with multiple offers/languages/etc by making a string out of them
+    return render_template("menteeList.html", nomentees=False, menteelist=mentordict)
 
 # to show a list of mentors, temporary placement just so i can test stuff
 @app.route("/mentors")
 def showMentorList():
     mentordict = [{ "twitterhandle": "fluffyunicorn", "languages": "javascript, python, ruby", "skills": "tight-rope walking, laughter", "offers": "getting started, happy unicorns", "originaltweet": "http://twitter.com/anyuser/status/203490203491094", "twitterprofile": "https://twitter.com/intent/user?user_id=23492" }, { "twitterhandle": "madeilenel", "languages": "javascript, python", "offers": "getting started, fika", "skills": "tea breaking, cartwheels", "originaltweet": "http://twitter.com/anyuser/status/203490203491094", "twitterprofile": "https://twitter.com/intent/user?user_id=23492" }]
     # TO DO: adapt this to work with multiple offers/languages/etc by making a string out of them
-    return render_template("mentorlist.html", nomentors=False, mentorlist=mentordict)
+    return render_template("mentorList.html", nomentors=False, mentorlist=mentordict)
 
 if __name__ == "__main__":
     app.run()
