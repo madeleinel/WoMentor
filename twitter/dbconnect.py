a6dca03fb40a736a52509a4970a6e9e63a8641c4
eleanor keane <egjkeane@gmail.com>
2017-04-04 13:54:04 +0100

    update dbconnect with new table defs

--- twitter/dbconnect.py
+++ twitter/dbconnect.py
@@ -11,43 +11,55 @@ session = Session()
 # session.execute(...)
 Base = declarative_base()
 
+map_table_language = Table('user_language_map_table',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('language_id', Integer, ForeignKey('language.id'))
+)
+
+map_table_skill = Table('user_skill_map_table',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('skill_id', Integer, ForeignKey('skill.id'))
+)
+
+map_table_offer = Table('user_offer_map_table',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('offer_id', Integer, ForeignKey('offer.id'))
+)
+
 class User(Base):
     __tablename__ = 'user'
-    uid = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     mentor_mentee = Column(String(100), nullable=False)
     twitter_uid = Column(String(100), nullable=False)
     original_tweet_id = Column(String(100), nullable=False)
     scrn_name = Column(String(20), nullable=False)
-    offer = relationship('Offer', backref='useroffer', lazy='dynamic')
-    skills = relationship('Skills', backref='skillset', lazy='dynamic')
-    languages = relationship('Languages', backref='polyglot', lazy='dynamic')
+    languages = relationship('Language', secondary=map_table_language, backref='users')
+    skills = relationship('Skill', secondary=map_table_skill, backref='users')
+    offers = relationship('Offer', secondary=map_table_offer, backref='users')
+
+class Language(Base):
+    __tablename__ = 'language'
+    id = Column(Integer, Sequence('language_id_seq'), primary_key=True)
+    language = Column(String(100), nullable=False)
+    __table_args__ = (
+        UniqueConstraint('language'),
+    )
+
+class Skill(Base):
+    __tablename__ = 'skill'
+    id = Column(Integer, Sequence('skill_id_seq'), primary_key=True)
+    skill = Column(String(100), nullable=False)
+    __table_args__ = (
+        UniqueConstraint('skill'),
+    )
 
 class Offer(Base):
     __tablename__ = 'offer'
-    oid = Column(Integer, primary_key=True)
-    offer_1 = Column(String(100))
-    offer_2 = Column(String(100))
-    offer_3 = Column(String(100))
-    user_id = Column(Integer, ForeignKey('user.uid'))
-
-class Skills(Base):
-    __tablename__ = 'skills'
-    oid = Column(Integer, primary_key=True)
-    skills_1 = Column(String(100))
-    skills_2 = Column(String(100))
-    skills_3 = Column(String(100))
-    skills_4 = Column(String(100))
-    user_id = Column(Integer, ForeignKey('user.uid'))
-
-class Languages(Base):
-    __tablename__ = 'languages'
-    oid = Column(Integer, primary_key=True)
-    languages_1 = Column(String(100))
-    languages_2 = Column(String(100))
-    languages_3 = Column(String(100))
-    languages_4 = Column(String(100))
-    user_id = Column(Integer, ForeignKey('user.uid'))
-
-# a_newuser = User(mentor_mentee="mentor", twitter_handle="faketwitterhandle")
-# session.add(a_newuser)
-# session.commit()
+    id = Column(Integer, Sequence('offer_id_seq'), primary_key=True)
+    offer = Column(String(100), nullable=False)
+    __table_args__ = (
+        UniqueConstraint('offer'),
+    )
