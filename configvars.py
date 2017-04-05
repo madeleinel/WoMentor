82c8333c55b1d71c22131d3d57144f9c0b3c390c
eleanor keane <egjkeane@gmail.com>
2017-04-05 18:12:36 +0100

    fix indent problem

--- configvars.py
+++ configvars.py
@@ -1,7 +1,6 @@
 import os
 
-database_url = os.environ['DATABASE_URL']
- = os.environ['test_var']
+database_url = os.environ['DATABASE_URL'] = os.environ['test_var']
 access_token = os.environ['TWIT_ACCESS_TOKEN']
 access_token_secret = os.environ['TWIT_ACCESS_TOKEN_SECRET']
 consumer_key = os.environ['TWIT_CONSUMER_KEY']
